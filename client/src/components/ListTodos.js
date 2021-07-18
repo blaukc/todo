2ad909a7ab7648587ad19334cdd9d3d97044c1e9
blaukc <brandonlau1@hotmail.com>
2021-07-18 15:22:03 +0800

    added color coding for if due date is overdue

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -5,27 +5,38 @@ const ListTodos = () => {
 
     const [todos, setTodos] = useState([]);
 
+    var overdue = []                                //array of whether each item is not due/due today/ovedue
+
     const getTodos = async() => {
         try {
             const response = await fetch("http://localhost:5000/todos");
             var jsonData = await response.json()
 
             jsonData = jsonData.sort((a, b) => {            //sorts due dates
-                console.log(a)
                 return new Date(a.due) - new Date(b.due)
             })
 
             for (let i = 0; i < jsonData.length; i++) {     //loops through dates to convert the date format
+                overdue.push({ color: "#000000"});          //sets default overdue as not due for all items
                 if (jsonData[i].due) {
-                    console.log(jsonData[i].due)
                     var d = new Date(jsonData[i].due);
-                    d = d.toLocaleDateString();             //converts from mmddyyyy to ddmmyyyy
-                    var monthDayYear = d.split("/")
+                    var monthDayYear = d.toLocaleDateString();             //converts from mmddyyyy to ddmmyyyy
+                    monthDayYear = monthDayYear.split("/")
                     var dayMonthYear = monthDayYear[1] + '/' + monthDayYear[0] + '/' + monthDayYear[2]
                     jsonData[i].due = dayMonthYear;
                 }
-            }
 
+                const today = new Date();       //get today's date and compare to due date
+                const todayDate = new Date(today.getFullYear(), today.getMonth(), today.getDate()); //keeps only date; removes time
+                if (todayDate.getTime() === d.getTime()) {      //getTime() to convert to epoch for comparison
+                    overdue[i] = { color: "#ffc107" }           //set style color as due today
+                } else if (todayDate.getTime() > d.getTime()) {
+                    overdue[i] = { color: "#dc3545" }           //set style color as overdue
+                }
+
+                jsonData[i].overdue = overdue[i];       //adds overdue to todos dict
+            }
+            console.log(jsonData)
             setTodos(jsonData)
         } catch(err) {
             console.error(err.message)
@@ -64,7 +75,9 @@ const ListTodos = () => {
                     {todos.map(todo => (
                         <tr key={todo.todo_id}>
                             <td>{todo.description}</td>
-                            <td>{todo.due}</td>
+                            <td>
+                                <p style={todo.overdue}>{todo.due}</p>
+                            </td>
                             <td>
                                 <EditTodo todo={todo}/>
                             </td>
